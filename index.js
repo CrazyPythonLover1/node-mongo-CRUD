5161f26bc198bc99ca89a86a6c1750e7cf0f4f8c
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-01 02:00:06 +0600

    Make CRUD operation working Smoothly

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ client.connect(err => {
     productCollection.insertOne(product)
     .then(result =>{
         console.log('One product added')
-        res.send("success");
+        //res.send("success");
+        res.redirect('/')
     })
   })
 
@@ -50,14 +51,14 @@ client.connect(err => {
       $set: {price: req.body.price, quantity: req.body.quantity}
     })
     .then(result => {
-      console.log(result);
+      res.send(result.modifiedCount > 0)
     })
   })
 
   app.delete('/delete/:id', (req, res) => {
     productCollection.deleteOne({_id: ObjectId(req.params.id)})
     .then((result) => {
-        console.log(result);
+        res.send(result.deletedCount > 0);
     })
   })
   //client.close();
